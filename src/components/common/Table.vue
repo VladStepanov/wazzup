4e1eac8cb95d90b52c748e01dd6e7dca112b3f3e
Vladislav Stepanov <dsfcoll@gmail.com>
2020-11-21 10:28:06 +0200

    init

---
+++ src/components/common/Table.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="table">
+    <table>
+      <thead>
+        <tr>
+          <td>first head</td>
+          <td>second head</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>first</td>
+          <td>second</td>
+        </tr>
+        <tr>
+          <td>row 2</td>
+          <td>row 3</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <button>Load users table</button>
+    <Table />
+  </div>
+</template>
+
+<script>
+import Table from '@/components/common/Table'
+
+export default {
+  name: 'Home',
+  components: { Table }
+}
+</script>
