3ac221fbf988031093703389525e4bc87acefb02
Vladislav Stepanov <dsfcoll@gmail.com>
2020-11-21 12:47:12 +0200

    display fetched users

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { Promised } from 'vue-promised'
 
 Vue.config.productionTip = false
+
+Vue.component('Promised', Promised)
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,75 @@
 <template>
   <div class="home">
-    <button>Load users table</button>
-    <Table />
+    <button @click="fetchUsers">Load users table</button>
+    <Promised :promise="usersFetchingPromise">
+      <template #pending>
+        <Loader />
+      </template>
+      <template>
+        <div class="table__wrapper">
+          <Table :rows="users" :headers="headers">
+            <!-- if we wanna style specific col -->
+            <template #col.email="{ row }">{{ row.email }}</template>
+          </Table>
+        </div>
+      </template>
+      <template #rejected="error">
+        {{ error.message }}
+      </template>
+    </Promised>
   </div>
 </template>
 
 <script>
+import Loader from '@/components/common/Loader'
 import Table from '@/components/common/Table'
+import { mapState } from 'vuex'
 
 export default {
   name: 'Home',
-  components: { Table }
+  components: { Loader, Table },
+  data: () => ({
+    usersFetchingPromise: null,
+    headers: [
+      {
+        label: 'Id',
+        field: 'id'
+      },
+      {
+        label: 'Полное имя',
+        field: 'fullname'
+      },
+      {
+        label: 'Компания',
+        field: 'company'
+      },
+      {
+        label: 'Почта',
+        field: 'email'
+      },
+      {
+        label: 'Юзернейм',
+        field: 'uname'
+      }
+    ]
+  }),
+  computed: {
+    ...mapState({
+      users: state => state.usersModule.users
+    })
+  },
+  methods: {
+    fetchUsers () {
+      this.usersFetchingPromise = this.$store.dispatch('usersModule/fetchUsers')
+    }
+  }
 }
 </script>
+
+<style scoped lang="scss">
+.table__wrapper {
+  margin-top: 24px;
+  display: flex;
+  justify-content: center;
+}
+</style>
